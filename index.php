5f0dcc01c045d745cc0974df8419512ed68ef90b
Nikita Kravtsov <kravtsovnikitast@mail.ru>
2021-09-23 19:13:57 +0300

    First take use git by atom

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Главная</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+    <?php require "navbar.html"?>
+  </head>
+  <body>
+
+<p> fjgfjfij</p>
+
+    <!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Tickets</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="#">Главная</a>
+          <a class="nav-link" href="#">Регистрация</a>
+          <a class="nav-link" href="#">Авторизация</a>
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+-->
+  </body>
+</html>
+<!-- https://www.youtube.com/watch?v=eCItZh6uMVc&t=1842s
+    https://www.youtube.com/watch?v=SPWEJTTsFj0&t=145s
+-->
